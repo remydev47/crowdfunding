c1ee0b4031d39db037bc8cd3fd43cfbcf94f0977
remydev47 <brianrimidev47@gmail.com>
2023-04-24 14:23:00 +0300

    added a comma

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 url: 'https://rpc.ankr.com/eth_goerli',
                 accounts: [`0x${process.env.PRIVATE_KEY}`]
             }
-          }
+          },
     settings: {
       optimizer: {
         enabled: true,
